c2e25c92d56e28d295c82b69da1a3861215ed8ab
Hestaine <ariffhestaine@gmail.com>
2024-02-12 21:53:09 +0100

    jkjnlklk

--- starter/script.js
+++ starter/script.js
@@ -253,7 +253,6 @@ console.log(jonas.getSummary());
 console.log(jonas.jonasSummary);
 console.log(jonas.jonasSummary);
 console.log(jonas.jonasSummary);
-*/
 
 // console.log("Lifting weights repitition 1🏋️‍♂️");
 // console.log("Lifting weights repitition 2🏋️‍♂️");
@@ -270,3 +269,101 @@ console.log(jonas.jonasSummary);
 for (let rep = 1; rep <= 30; rep++) {
   console.log(`Lifting weights repitition ${rep}🏋️‍♂️`);
 }
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+// console.log(jonas[0]);
+// console.log(jonas[1]);
+// ...
+// console.log(jonas[4]);
+// Jonas[5] does not exist
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log("--- Only Srtings ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log("--- Break With Number ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------ Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weight repititon ${rep} 🏋️‍♂️`);
+  }
+}
+
+// for (let i = 1; i < 11; i++) {
+//   for (let i = 1; i < 11; i++) {
+//     console.log(`Lifting weight counter ${i} 🏋️‍♂️`);
+//   }
+// }
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repitition ${rep}🏋️‍♂️`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`While: Lifting weights repitition ${rep}🏋️‍♂️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log(`You rolled a ${dice}`);
+}
+*/
